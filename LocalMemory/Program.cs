c6598d3c2e10614d93e407325b9eecaee408d3ec
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-13 17:59:26 -0500

    Adds: Local memory exercise

---
+++ LocalMemory/Program.cs
@@ -0,0 +1,15 @@
+﻿new Thread(PrintNumbers)
+{
+    Name = "Worker Thread" // Naming threads (handy for debugging.)
+}.Start(); // Worker Thread
+
+Thread.CurrentThread.Name = "Main Thread";
+
+PrintNumbers();
+
+void PrintNumbers(){
+    for (var i = 0; i < 30; i++) // Each thread will have it's own copy of i.
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name}: {i}");
+    }
+}
