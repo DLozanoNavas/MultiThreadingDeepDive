46beaebdf0e27dfbb48f79039b095513a003b985
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-13 18:55:38 -0500

    Adds: ExceptionHandling Exercise.

---
+++ ThreadExceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿Thread.CurrentThread.Name = "Main Thread";
+
+Demo();
+Demo2();
+
+void Demo()
+{
+    try
+    {
+        new Thread(Execute)
+        {
+            Name = "Worker Thread"
+        }.Start();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message); // Exception won't be caught here because exception handling is done on a per-thread basis.
+    }
+}
+
+void Execute()
+{
+    throw null;
+}
+
+void Demo2()
+{
+
+    new Thread(Execute2)
+    {
+        Name = "Worker Thread"
+    }.Start();
+
+}
+
+void Execute2()
+{
+    try
+    {
+        throw null;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message); // Exception will get caught here because exception handling is done on a per-thread basis.
+    }
+}
