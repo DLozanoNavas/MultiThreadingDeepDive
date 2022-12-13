45628279ef43ff1e0ed5eee8c07af34486bad2c4
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-13 18:44:08 -0500

    Adds: Thread Join exercise.

---
+++ ThreadJoin/Program.cs
@@ -0,0 +1,16 @@
+﻿Thread.CurrentThread.Name = "Main Thread";
+
+var thread = new Thread(WriteUsingNewThread)
+{
+    Name = "Worker Thread" // Naming threads (handy for debugging.)
+}; // Worker Thread
+thread.Start();
+thread.Join(); // Wait for the thread to finish.
+
+Console.WriteLine("Application Finished.");
+
+void WriteUsingNewThread()
+{
+    Console.WriteLine("Hello World printed using thread: {0}.", Thread.CurrentThread.Name);
+    Thread.Sleep(5000); // Simulate a long running task.
+}
