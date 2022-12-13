b41456fa936a91a727713d7d2cec920561fd97ce
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-13 17:59:10 -0500

    Adds: Shared Resources exercise.

---
+++ SharedResources/Program.cs
@@ -0,0 +1,22 @@
+﻿Thread.CurrentThread.Name = "Main Thread";
+bool isCompleted = false;
+object lockCompleted = new object();
+
+var thread = new Thread(WriteHelloWorld)
+{
+    Name = "Worker Thread" // Naming threads (handy for debugging.)
+}; // Worker Thread
+thread.Start();
+WriteHelloWorld();
+
+void WriteHelloWorld()
+{
+    lock (lockCompleted)
+    {
+        if (isCompleted) return;
+
+        Console.WriteLine($"Hello world using thread: {Thread.CurrentThread.Name}.");
+        
+        isCompleted = true;
+    }
+}
