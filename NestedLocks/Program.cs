de82d3f24dd3f96e96b8bfbd6ed1c1e43bfaa747
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-15 00:49:07 -0500

    Adds: Nested lock example

---
+++ NestedLocks/Program.cs
@@ -0,0 +1,27 @@
+﻿// When we have nested locks, only the most outer lock us relevant.
+
+object lockObject = new object();
+
+lock (lockObject)
+{
+    OtherStuff();
+}
+
+void OtherStuff()
+{
+    lock (lockObject)
+    {
+        Task.Delay(1000);
+        // Do something
+        MoreStuff();
+    }
+}
+
+void MoreStuff()
+{
+    lock (lockObject)
+    {
+        // Do something
+        Console.WriteLine("Hello World");
+    }
+}
