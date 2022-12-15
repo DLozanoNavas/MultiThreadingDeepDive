396e21ce09e7ae05a4dee74e61f67617959a0b9b
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-14 23:18:28 -0500

    Adds: Tasks Intro Exercise.

---
+++ TasksIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿var task = new Task(SimpleMethod);
+task.Start();
+
+var taskThatReturns = new Task<string>(MethodThatReturns);
+taskThatReturns.Start();
+taskThatReturns.Wait();
+var blockingResult =  taskThatReturns.Result;
+Console.WriteLine(blockingResult);
+
+var nonBlockingResult = await taskThatReturns;
+Console.WriteLine(nonBlockingResult);
+
+Console.ReadLine();
+
+void SimpleMethod()
+{
+    Console.WriteLine("Hello from task");
+}
+
+string MethodThatReturns()
+{
+    Task.Delay(5000);
+    return "Hello from method that returns";
+}
