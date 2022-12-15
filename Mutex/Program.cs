9eeefb9c984a315c1936a9a3a3cade8099b6c2a3
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-15 01:03:37 -0500

    Adds: Mutex example.

---
+++ Mutex/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program
+{
+    static Mutex mutex = new(false, "Mutex");
+    
+    static void Main(string[] args)
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            new Thread(AcquireMutex){Name = $"Thread {i}"}.Start();
+        }
+    }
+
+    static void AcquireMutex()
+    {
+        mutex.WaitOne(); // Wait until it is safe to enter.
+        Console.WriteLine($"{Thread.CurrentThread.Name} has entered the protected area");
+        Thread.Sleep(1000);
+        Console.WriteLine($"{Thread.CurrentThread.Name} is leaving the protected area");
+        mutex.ReleaseMutex(); // Release the Mutex.
+    }
+}
