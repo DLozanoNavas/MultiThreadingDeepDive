5cbe3be85ef6c680712cf759244f25ae92e775ad
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-14 23:18:38 -0500

    Adds: Tasks IO Exercise.

---
+++ IOTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿var task = Task.Factory.StartNew(PrintPosts);
+
+task.Wait(); // This way, we can choose when to wait for the task.
+SomethingElse();
+
+
+Console.ReadLine();
+
+string GetPosts(string url)
+{
+    //throw null; // simulate error
+    using var httpClient = new HttpClient();
+    return httpClient.GetStringAsync(url).Result;
+}
+
+void PrintPosts()
+{
+    var postsTask = Task.Factory.StartNew<string>(() => GetPosts("https://jsonplaceholder.typicode.com/posts"));
+
+    try
+    {
+        Console.WriteLine(postsTask.Result); // When querying task.result, the current thread will wait until the task finishes.
+    }
+    catch (AggregateException e) // if any exception happens When querying task.Result, the exception it will be rethrown wrapped into an AggregateException
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void SomethingElse()
+{
+    Task.Delay(2000);
+    Console.WriteLine("Something else happened.");
+}
