d60b8ddc312ae29f6b8ace0e6e568e84608a0451
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-15 00:46:38 -0500

    Adds: Deadlock example

---
+++ DeadLock/Program.cs
@@ -0,0 +1,35 @@
+﻿object firstLock = new object();
+object secondLock = new object();
+
+new Thread(FirstMethod) { Name = "First Thread" }.Start();
+new Thread(SecondMethod) { Name = "Second Thread" }.Start();
+
+Console.ReadLine();
+
+// This method will be called by the first thread.
+void FirstMethod()
+{
+    lock (firstLock)
+    {
+        Console.WriteLine("First thread acquired first lock");
+        Thread.Sleep(1000);
+        lock (secondLock)
+        {
+            Console.WriteLine("First thread acquired second lock");
+        }
+    }
+}
+
+// This method will be called by the second thread.
+void SecondMethod()
+{
+    lock (secondLock)
+    {
+        Console.WriteLine("Second thread acquired second lock");
+        Thread.Sleep(1000);
+        lock (firstLock)
+        {
+            Console.WriteLine("Second thread acquired first lock");
+        }
+    }
+}
