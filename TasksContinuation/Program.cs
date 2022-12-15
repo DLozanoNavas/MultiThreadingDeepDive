968e8b33c78111842de6026035a4b09208d939c1
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-14 23:18:47 -0500

    Adds: Tasks Continuation Exercise.

---
+++ TasksContinuation/Program.cs
@@ -0,0 +1,22 @@
+﻿// Continuation: Asynchronous task invoked by another task called an antecedent.
+// Task chaining allows to:
+// - Pass data from an antecedent to the continuation task
+// - Pass exceptions to antecedent to continuation task (for continuation to handle)
+// - Control how the continuation is invoked
+// - Cancel a continuation not only before it starts, but also while the continuation is running.
+// - Invoke multiple continuations from the same antecedent
+// - Invoke continuations when all or any of the antecedents complete (WhenAll/WhenAny)
+
+var antecedent = Task.Factory.StartNew(() =>
+{
+    Task.Delay(2000);
+    return DateTime.Today.ToShortDateString();
+});
+
+var continuation = antecedent.ContinueWith(t =>
+{
+    Task.Delay(1000);
+    return $"Today is {t.Result}";
+});
+
+Console.WriteLine(continuation.Result);
