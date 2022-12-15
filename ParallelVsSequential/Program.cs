59a9673a2c20f7b5a792a729f38f2130d556b9c0
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-15 16:22:11 -0500

    Adds: Parallel vs Sequential example

---
+++ ParallelVsSequential/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var originalFilesPath = Path.Join(Directory.GetCurrentDirectory(), "pictures");
+        var modifiedFilesPath = Path.Join(Directory.GetCurrentDirectory(), "alteredPictures");
+        Directory.CreateDirectory(originalFilesPath);
+        Directory.CreateDirectory(modifiedFilesPath);
+
+        var files = Directory.GetFiles(originalFilesPath, "*.jpg", SearchOption.AllDirectories);
+        
+        ParallelExecution(files, modifiedFilesPath);
+
+        NormalExecution(files, modifiedFilesPath);
+
+    }
+
+    static void ParallelExecution(string[] files, string modifiedFilesPath)
+    {
+        Stopwatch stopWatch = new();
+        stopWatch.Start();
+        Parallel.ForEach(files, file =>
+        {
+            var fileName = Path.GetFileName(file);
+            var newFileName = Path.Join(modifiedFilesPath, fileName);
+            using var fileBitMap = new Bitmap(file);
+            fileBitMap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            fileBitMap.Save(newFileName);
+        });
+        
+        stopWatch.Stop();
+        Console.WriteLine("Parallel.ForEach took: " + stopWatch.ElapsedMilliseconds + "ms");
+    }
+
+    static void NormalExecution(string[] files, string modifiedFilesPath)
+    {
+        Stopwatch stopWatch = new();
+        stopWatch.Start();
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file);
+            var newFileName = Path.Join(modifiedFilesPath, fileName);
+            using var fileBitMap = new Bitmap(file);
+            fileBitMap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            fileBitMap.Save(newFileName);
+        }
+        stopWatch.Stop();
+        Console.WriteLine("Normal execution took: " + stopWatch.ElapsedMilliseconds + "ms");
+    }
+}
---
+++ TPL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopWatch = new();
+        
+        for (var i = 0; i < 100000; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+        stopWatch.Stop();
+        Console.WriteLine("For loop took: " + stopWatch.ElapsedMilliseconds + "ms");
+        stopWatch.Restart();
+        
+        Console.WriteLine("---------------------------------");
+        Parallel.For(0, 100000, Console.WriteLine);
+        Console.WriteLine("Parallel.For took: " + stopWatch.ElapsedMilliseconds + "ms");
+    }
+}
